d2fc65dcd86d1ce7180111d90e487b79d4a36763
Philip Ray <dad@Macmini.lan>
2025-04-15 14:47:58 -0700

    Fix MongoDB connection string

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 console.log('MONGO_URI:', process.env.MONGO_URI);
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Failed to connect to MongoDB:', err));
 
 console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
